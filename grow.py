9d8adb05314c581ea7d3a912af4182e2a7b24a0c
Laura Galan <galanlaurag@gmail.com>
2021-12-06 00:40:59 +0000

    Added new files

--- grow.py
+++ grow.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request
+import sqlite3 as sql
+
 app = Flask(__name__)
+con = sql.connect('var/database.db')
+con.execute('CREATE TABLE IF NOT EXISTS tbl_form (ID INTEGER PRIMARY KEY AUTOINCREMENT, ' + 'name TEXT, surname TEXT, email TEXT, reason TEXT, phone TEXT, date DATE, time TIME, message TEXT, comment TEXT)')
 
 @app.route('/')
 def root():
@@ -13,28 +17,47 @@ def gallery():
 def about():
         return render_template('about.html'), 200
 
+#source: https://www.codegrepper.com/code-examples/python/how+to+save+form+data+to+database+in+flask%5C
 @app.route('/contact/', methods=["GET", "POST"])
 def contact():
-
-	if request.method == "POST":
+	if request.method == "GET":
+		return render_template('contact.html'), 200
+	else:
 		req = request.form
 		name = req["firstname"]
 		surname = req["lastname"]
 		email = req["email"]
 		reason = req["reason"]
-		telephone = req["phone"]
+		phone = req["phone"]
 		date = req["date"]
 		time = req["time"]
 		message = req["message"]
 		comment = req["comment"]
-		print(name, surname, email, reason, telephone, date, time, message, comment)
-		return redirect(request.url)        
-
-	return render_template('contact.html'), 200
+		print(name, surname, email, reason, phone, date, time, message, comment)
+		try:
+			con = sql.connect('var/database.db')
+			c = con.cursor()
+			c.execute("INSERT INTO tbl_form (name, surname, email, reason, phone, date, time, message, comment) VALUES(?,?,?,?,?,?,?,?,?)", 
+				(name , surname , email , reason , phone , date , time , message , comment))
+			con.commit()
+			return render_template('formsuccess.html'), 200
+		except con.Error:
+			return render_template('formerror.html'), 200
+		finally:
+			con.close()
 
 @app.route('/menu/')
 def menu():
         return render_template('menu.html'), 200
+
+@app.route('/formsuccess/')
+def success():
+        return render_template('formsuccess.html'), 200
+
+@app.route('/formerror/')
+def error():
+        return render_template('formerror.html'), 200
+
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -65,15 +65,16 @@ drinks.classList.add("active");
 }
 
 
+
 // flexible form
 $("#reason").change(function() {
   if ($(this).val() != "booking") {
     $('.phone').hide();
     $('#phone').removeAttr('required');
     $('.date').hide();
-    $('#date').removeAttr('required');
+    $('#datefield').removeAttr('required');
     $('.time').hide();
-    $('#time').removeAttr('required');
+    $('#timefield').removeAttr('required');
     $('.comment').hide();
     $('.message').show();
     $('#message').attr('required');  
@@ -81,32 +82,19 @@ $("#reason").change(function() {
     $('.phone').show();
     $('#phone').attr('required');
     $('.date').show();
-    $('#date').attr('required');
+    $('#datefield').attr('required');
     $('.time').show();
-    $('#time').attr('required');
+    $('#timefield').attr('required');
     $('.comment').show();
     $('.message').hide();
     $('#message').removeAttr('required');
-//  });
-//$("#reason").change(function() {
-//if ($(this).val() == "general" || "other") {
-  //  $('.phone').hide();
-   // $('#phone').removeAttr('required');
-  //  $('.date').hide();
-  //  $('#date').removeAttr('required');
-  //  $('.time').hide();
-  //  $('#time').removeAttr('required');
-  //  $('.comment').hide();
-  //  $('.message').show();
-  //  $('#message').attr('required');
-//    $('#message').attr('data-error', 'This field is required.');
   }
 });
 $("#reason").trigger("change");
 
 
 //adjusted time selection
-// source: https://timepicker.co
+//source: https://timepicker.co
 $(document).ready(function(){
   $('#timefield').timepicker({
       timeFormat: 'h:mm a',
@@ -148,101 +136,4 @@ function hamburgerMenuFunction() {
   }
 }
 
-// As A Vanilla JavaScript Plugin
-var gallery = new SimpleLightbox('.gallery a', {
-    /* options */
-// shows fullscreen overlay
-overlay: true,
-
-// shows navigation arrows
-nav: true,
-
-// shows close button
-close: true,
-
-// text for close button
-closeText: 'X',
-
-// swipe up or down to close gallery
-swipeClose: true,
-
-// file extensions
-fileExt: 'png|jpg|jpeg|gif',
-
-// weather to slide in new photos or not, disable to fade
-animationSlide: true,
-
-// animation speed in ms
-animationSpeed: 250,
-
-// image preloading
-preloading: true,
-
-// keyboard navigation
-enableKeyboard: true,
-
-// endless looping
-loop:  true,
-
-// group images by rel attribute of link with same selector
-//rel: false,
-
-// closes the lightbox when clicking outside
-docClose:  true,
-
-// how much pixel you have to swipe
-swipeTolerance: 50,
-
-// lightbox wrapper Class
-className: 'simple-lightbox',
-
-// width / height ratios
-//widthRatio: 0.8,
-//heightRatio: 0.9,
-
-// scales the image up to the defined ratio size
-//scaleImageToRatio: false,
-
-// disable right click
-disableRightClick: false,
-
-// disable page scroll
-//disable<a href="https://www.jqueryscript.net/tags.php?/Scroll/">Scroll</a>: true,
-
-// show an alert if image was not found
-alertError:  true,
-
-// alert message
-alertErrorMessage: 'Image not found, next image will be loaded',
-
-
-// enable history back closes lightbox instead of reloading the page
-history: true,
-
-// time to wait between slides
-throttleInterval: 0,
-
-// Pinch to <a href="https://www.jqueryscript.net/zoom/">Zoom</a> feature for touch devices
-doubleTapZoom: 2,
-maxZoom: 10,
-
-// adds class to html element if lightbox is open
-//htmlClass: 'has-lightbox',
-
-
-
-// fade speed in ms
-fadeSpeed: 300,
-
-// whether to uniqualize images
-uniqueImages: true,
-
-// focus the lightbox on open to enable tab control
-focus: true,
-});
-
-// As A jQuery Plugin -->
-/*var gallery = $('.gallery a').simpleLightbox({
-    /* options */
-/*});*/
 
