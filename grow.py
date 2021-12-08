26da04a2662910477a32699a3c80e11a2794e4fe
Laura Galan <galanlaurag@gmail.com>
2021-12-08 18:11:45 +0000

    folder back

---
+++ grow.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, url_for, request
+from flask_recaptcha import ReCaptcha 
+import sqlite3 as sql
+
+app = Flask(__name__)
+con = sql.connect('var/database.db')
+con.execute('CREATE TABLE IF NOT EXISTS tbl_form (ID INTEGER PRIMARY KEY AUTOINCREMENT, ' + 'name TEXT, surname TEXT, email TEXT, reason TEXT, phone TEXT, date DATE, time TIME, message TEXT, comment TEXT)')
+
+#source: https://ichi.pro/pl/jak-korzystac-z-google-recaptcha-z-flask-241719104676243
+app.config['RECAPTCHA_SITE_KEY'] = '6LdBz34dAAAAAJKIZGQk7J4e5rcvrnuxphO5Y1_U'
+app.config['RECAPTCHA_SECRET_KEY'] = '6LdBz34dAAAAAKeCUdcN0XOcx8_NSFSinbuuMSJM'
+recaptcha = ReCaptcha(app)
+
+@app.route('/')
+def root():
+	return render_template('index.html'), 200
+
+@app.route('/gallery/')
+def gallery():
+	return render_template('gallery.html'), 200
+
+@app.route('/about/')
+def about():
+        return render_template('about.html'), 200
+
+#source: https://www.codegrepper.com/code-examples/python/how+to+save+form+data+to+database+in+flask%5C
+@app.route('/contact/', methods=["GET", "POST"])
+def contact():
+	if request.method == "GET":
+		return render_template('contact.html'), 200
+	else:
+		req = request.form
+		name = req["firstname"]
+		surname = req["lastname"]
+		email = req["email"]
+		reason = req["reason"]
+		phone = req["phone"]
+		date = req["date"]
+		time = req["time"]
+		message = req["message"]
+		comment = req["comment"]
+		print(name, surname, email, reason, phone, date, time, message, comment)
+		try:
+			con = sql.connect('var/database.db')
+			c = con.cursor()
+			c.execute("INSERT INTO tbl_form (name, surname, email, reason, phone, date, time, message, comment) VALUES(?,?,?,?,?,?,?,?,?)", (name , surname , email , reason , phone , date , time , message , comment))				
+			con.commit()
+			return render_template('formsuccess.html'), 200
+		except con.Error:
+			return render_template('formerror.html'), 200
+		finally:
+			con.close()
+
+@app.route('/menu/')
+def menu():
+        return render_template('menu.html'), 200
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return render_template('error.html'), 404
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,167 @@
+//dark mode
+function onload() {
+  document.getElementsByTagName('html')[0].classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+  document.querySelector(".mode_img").addEventListener('click', manageMode);
+
+  function manageMode() {
+    var mode = localStorage.getItem('darkmode') === 'true';
+    localStorage.setItem('darkmode', !mode);
+    document.getElementsByTagName('html')[0].classList.toggle('dark-mode', !mode);
+  }
+}
+
+//top button
+//sources: https://codepen.io/matthewcain/pen/ZepbeR, https://www.geeksforgeeks.org/how-to-animate-scrolltop-using-jquery/
+$(window).scroll(function() {
+  var btn = $('#myBtn');
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+function topFunction() {
+  $("html, body").animate(
+    { scrollTop: "0" }, 1000);
+}
+
+
+//active (underlined) navmenu elements
+function juices() {
+var juices = document.getElementById("onclickjuices");
+juices.classList.add("active");
+var shakes = document.getElementById("onclickshakes");
+shakes.classList.remove("active");
+var bowls = document.getElementById("onclickbowls");
+bowls.classList.remove("active");
+var savoury = document.getElementById("onclicksavoury");
+savoury.classList.remove("active");
+var drinks = document.getElementById("onclickdrinks");
+drinks.classList.remove("active");
+}
+
+function shakes() {
+var juices = document.getElementById("onclickjuices");
+juices.classList.remove("active");
+var shakes = document.getElementById("onclickshakes");
+shakes.classList.add("active");
+var bowls = document.getElementById("onclickbowls");
+bowls.classList.remove("active");
+var savoury = document.getElementById("onclicksavoury");
+savoury.classList.remove("active");
+var drinks = document.getElementById("onclickdrinks");
+drinks.classList.remove("active");
+}
+
+function bowls() {
+var juices = document.getElementById("onclickjuices");
+juices.classList.remove("active");
+var shakes = document.getElementById("onclickshakes");
+shakes.classList.remove("active");
+var bowls = document.getElementById("onclickbowls");
+bowls.classList.add("active");
+var savoury = document.getElementById("onclicksavoury");
+savoury.classList.remove("active");
+var drinks = document.getElementById("onclickdrinks");
+drinks.classList.remove("active");
+}
+
+function savoury() {
+var juices = document.getElementById("onclickjuices");
+juices.classList.remove("active");
+var shakes = document.getElementById("onclickshakes");
+shakes.classList.remove("active");
+var bowls = document.getElementById("onclickbowls");
+bowls.classList.remove("active");
+var savoury = document.getElementById("onclicksavoury");
+savoury.classList.add("active");
+var drinks = document.getElementById("onclickdrinks");
+drinks.classList.remove("active");
+}
+
+function drinks() {
+var juices = document.getElementById("onclickjuices");
+juices.classList.remove("active");
+var shakes = document.getElementById("onclickshakes");
+shakes.classList.remove("active");
+var bowls = document.getElementById("onclickbowls");
+bowls.classList.remove("active");
+var savoury = document.getElementById("onclicksavoury");
+savoury.classList.remove("active");
+var drinks = document.getElementById("onclickdrinks");
+drinks.classList.add("active");
+}
+
+
+
+// flexible form
+$("#reason").change(function() {
+  if ($(this).val() != "booking") {
+    $('.phone').hide();
+    $('#phone').removeAttr('required');
+    $('.date').hide();
+    $('#datefield').removeAttr('required');
+    $('.time').hide();
+    $('#timefield').removeAttr('required');
+    $('.comment').hide();
+    $('.message').show();
+    $('#message').attr('required');  
+ }else{
+    $('.phone').show();
+    $('#phone').attr('required');
+    $('.date').show();
+    $('#datefield').attr('required');
+    $('.time').show();
+    $('#timefield').attr('required');
+    $('.comment').show();
+    $('.message').hide();
+    $('#message').removeAttr('required');
+  }
+});
+$("#reason").trigger("change");
+
+
+//adjusted time selection
+//source: https://timepicker.co
+$(document).ready(function(){
+  $('#timefield').timepicker({
+      timeFormat: 'h:mm a',
+      interval: 30,
+      minTime: '8:00am',
+      maxTime: '5:30pm',
+      startTime: '8:00',
+      dynamic: false,
+      dropdown: true,
+      scrollbar: true,
+  });
+});
+
+// // todays min date form
+// // source: https://www.codegrepper.com/code-examples/html/input+type%3Ddate+min+today
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1;
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+  dd = '0'+dd;
+}
+if (mm < 10) {
+  mm = '0'+mm;
+}
+today = yyyy + '-' + mm + '-' + dd;
+document.getElementById("datefield").setAttribute("placeholder", today);
+document.getElementById("datefield").setAttribute("min", today);
+
+//hamburger mobile menu 
+function hamburgerMenuFunction() {
+  var x = document.getElementById("grownavbar");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
+}
+
+
+
